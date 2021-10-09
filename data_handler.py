9dde0f2445c3d92f764cd0d636693e33bfdfd62b
jironymo jirolamus <greedisneutral@gmail.com>
2021-10-09 22:47:30 +0300

    reorganised handling data as a general-purpose class

--- data_handler.py
+++ data_handler.py
@@ -1,30 +1,69 @@
-import csv
+"""
+Provides access to the Data_handler class, which is basically a general-purpose wrapper around a db file
+with the following core self-explanatory methods of interest: 
+    1. fetch_param(self, dependent_param, independent_param, value);
+    2. append_entry(self, param_dict);
+"""
 
-def fetch_price(param_name, value):
+class Data_handler:
     """
-    Expected name of the parameter and its corresponding value for rows to be tested against.
-    Possibly will be reorganised into a class to hold temporary values when the architecture is decided on.
+    Initializes object of Data_handler type.
+    Expects path to a data file as an initialization input param.
+    Data_handler obj allows storing data files in-memory, fetch data based on provided params, put data into a db file.   
     """
-    pass
+    def __init__(self, fpath):
+        import csv
+        self._fpath = fpath
 
-    with open('housing_data/train.csv', 'r', newline='') as csv_fdata:
-        csv_reader = csv.DictReader(csv_fdata)
-        line_count = 0
-        price_list = []
-        for row in csv_reader:
-            if line_count == 0:
-                header = row
+        # store header of the data file
+        with open(self._fpath, 'r', newline='') as _csv_fdata:
+            self._csv_reader = csv.reader(_csv_fdata)
+            self._data_header = next(self._csv_reader)
+
+        # in-memory representation of data since it is not too heavy
+        with open(self._fpath, 'r', newline='') as _csv_fdata:
+            self._csv_dictreader = csv.DictReader(_csv_fdata)
             
-            if row[param_name] == value:
-                price_list.append(row['price_doc'])
-            line_count += 1
-    # print(f'Processed {line_count} lines.')
-    return price_list
+            # _data_inm is a list with the following structure:
+            # [dict({*param11*:*val11*, *param12*:*val12*, ...}, dict({*param21*:*val21*, *param22*:*val22*, ...})]
+            self._data_inm = []
+            for row in self._csv_dictreader:
+                self._data_inm.append(row) 
 
-def put_data(param_dict):
-    """expected a dict of pairs *param*: *value*"""
-    pass
+            self._id_max = self._data_inm[-1]['id']
 
-    with open('housing_data/train.csv', 'a', newline='') as csv_fdata:
-        csv_writer = csv.DictWriter(csv_fdata)
-        csv_writer.writerow(param_dict)
+    def __repr__(self):
+        return self._data_inm
+
+    @property
+    def data(self):
+        # return self.__repr__()
+        return self._data_inm
+
+    @data.setter
+    def data(self):
+        print(f'use obj_name.append_entry method to add new row to the db.')
+
+    @data.deleter
+    def data(self):
+        raise AttributeError('Can not access this method.')
+
+    def fetch_param(self, dependent_param, independent_param, value):
+        """
+        Expects name of the parameter to be fetched (*dependent_param*), 
+        the *value* for *independent_param* to be tested against.
+        """
+        pass
+
+    def append_entry(self, param_dict):
+        """
+        Expected a dict of pairs *param*: *value*
+        """
+        pass
+
+# print(Data_handler._id_max)
+# print(Data_handler._data_header)
+
+homer = Data_handler('housing_data/train.csv')
+
+print(homer.data)
